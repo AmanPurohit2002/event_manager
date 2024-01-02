306f54b5b33b87578a2a8dcd6288d9fb162ebdcb
Aman_Purohit <amanpurohit785@gmail.com>
2024-01-02 14:45:44 +0530

    integerate the cleark app for user management

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        {/* <h1 className='text-4xl'>Evently</h1> */}
+      </div>
+    </main>
+
+  )
+}
